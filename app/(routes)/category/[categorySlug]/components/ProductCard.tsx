5859938ff2d08bf559384f1e82b037747e6988de
Leandro <leonicvarc@gmail.com>
2026-03-13 17:50:24 -0500

    Refactor card component and update styles

---
+++ app/(routes)/category/[categorySlug]/components/ProductCard.tsx
@@ -0,0 +1,58 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel"
+import { ProducType } from "@/types/product"
+import Image from "next/image"
+import Link from "next/link"
+
+type ProductCardProps = {
+  product: ProducType
+}
+
+export const ProductCard = (props: ProductCardProps) => {
+  const { product } = props
+  return (
+    <Link
+      href={`/product/${product.slug}`}
+      className="relative block rounded-lg p-2 transition-all duration-100 hover:shadow-lg"
+    >
+      <div className="absolute top-4 z-10 flex items-center justify-between gap-3 px-2">
+        <p className="w-fit rounded-full bg-black px-2 py-1 text-xs text-white dark:bg-white dark:text-black">
+          {product.taste}
+        </p>
+        <p className="w-fit rounded-full bg-yellow-900 px-2 py-1 text-xs text-white">
+          {product.origin}
+        </p>
+      </div>
+
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full max-w-sm"
+      >
+        <CarouselContent>
+          {product.images.map((image) => (
+            <CarouselItem key={image.id}>
+              <div className="h-60 overflow-hidden rounded-xl">
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${image.url}`}
+                  alt={product.productName}
+                  width={400}
+                  height={240}
+                  priority
+                  style={{ width: "100%", height: "auto" }}
+                  className="object-cover"
+                />
+              </div>
+
+              <div className="ab"></div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </Link>
+  )
+}
--- app/(routes)/category/[categorySlug]/page.tsx
+++ app/(routes)/category/[categorySlug]/page.tsx
@@ -1,6 +1,8 @@
 import { getCategoryProducts } from "@/api/getCategoryProduct"
 import { Separator } from "@/components/ui/separator"
 import { FiltersControlsCategory } from "./components/FiltersControlsCategory"
+import { ProductCard } from "./components/ProductCard"
+import { ProducType } from "@/types/product"
 
 type PageProps = {
   params: Promise<{ categorySlug: string }>
@@ -9,8 +11,6 @@ type PageProps = {
 export default async function Page({ params }: PageProps) {
   const { categorySlug } = await params
   const { data } = await getCategoryProducts(categorySlug)
-
-  console.log(data)
 
   return (
     <div className="mx-auto max-w-6xl py-4 sm:px-24 sm:py-16">
@@ -21,6 +21,13 @@ export default async function Page({ params }: PageProps) {
 
       <div className="sm:flex sm:justify-between">
         <FiltersControlsCategory />
+
+        {/* Suspense??? */}
+        <div className="mt-8 grid gap-5 md:grid-cols-3 md:gap-10">
+          {data.map((product: ProducType) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
       </div>
     </div>
   )
